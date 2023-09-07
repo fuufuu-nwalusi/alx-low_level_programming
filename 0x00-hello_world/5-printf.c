eedbfcc7d68ee21f9d56d784764196d6b6836ca5
Epique Buff <oliviaujunwa851@gmail.com>
2023-09-07 05:34:59 -0700

    Hello, Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,8 @@
 /**
  * main - Entry point
  *
- * return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
